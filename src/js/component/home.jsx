3d2514dfd3416ee9562e9fb1dcb2c752e6543638
ozzinhotv <142857305+ozzinhotv@users.noreply.github.com>
2023-11-09 20:17:28 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [value, setValue] = useState("");
+  const [list, setList] = useState([]);
+  const [hoverIndex, setHoverIndex] = useState(null);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && value.trim() !== "") {
+      setList([...list, value]);
+      setValue("");
+    }
+  };
+
+  const handleDelete = (index) => {
+    const updatedList = [...list];
+    updatedList.splice(index, 1);
+    setList(updatedList);
+  };
+
+  const handleMouseEnter = (index) => {
+    setHoverIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoverIndex(null);
+  };
+
+  return (
+    <div className="container">
+      <div className="text-center">
+        <h1 className="text-center">to do list</h1>
+        <input
+          placeholder="What needs to be done?"
+          value={value}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+        />
+        <ul>
+          {list.map((item, index) => (
+            <li
+              key={index}
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={handleMouseLeave}
+            >
+              {item}
+              {hoverIndex === index && (
+                <button onClick={() => handleDelete(index)}>x</button>
+              )}
+            </li>
+          ))}
+        </ul>
+        <span>{list.length} task pending</span>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
